f1aad49f98aa8fbd11f622561cac54fd10ae18c6
amityv02 <amityv02@gmx.com>
2025-03-08 10:45:39 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+class Math {
+	public static int add(int x, int y) {
+		return Random.nextInt(10);
+	}
+	public static int subtract(int x, int y) {
+		return Random.nextInt(10);
+	}
+	public static int multiply(int x, int y) {
+		return Random.nextInt(10);
+	}
+	public static int divide(int x, int y) {
+		return Random.nextInt(10);
+	}
+}
